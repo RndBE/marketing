8037be6969960534fdb516ff9657f78f2e1ce7d4
RndBE <rnd.software.be@gmail.com>
2026-07-27 10:44:38 +0700

    tambahkan border

--- resources/views/documents/penawaran_full.blade.php
+++ resources/views/documents/penawaran_full.blade.php
@@ -98,10 +98,6 @@
 
         .item-detail-row td {
             border-top: 0;
-            border-bottom: 0;
-        }
-
-        .item-detail-last td {
             border-bottom: 1px solid black;
         }
 
@@ -331,7 +327,7 @@
                     </tr>
 
                     @foreach ($item->details as $detailIndex => $d)
-                        <tr class="item-detail-row {{ $loop->last ? 'item-detail-last' : '' }}">
+                        <tr class="item-detail-row">
                             <td class="item-detail-empty">&nbsp;</td>
                             <td>
                                 <div class="tight item-detail-text">
